2203edf49d6b2891270dd35490a6b16c7be6bedf
FRAISSINET Clement <clement-fraissinet@orange.fr>
2019-02-07 09:44:55 +0100

    Add exercice4

--- exercice/exercice4.php
+++ exercice/exercice4.php
@@ -4,6 +4,19 @@
  * the standard input according to the problem statement.
  **/
 $s = $args[0];
+$tableau = str_split($s);
+$rond = 0;
+
+foreach ($tableau as $value) {
+    // If bidule = 6 || 9 || 0      =>      rond++
+    // If bidule = 8                =>      rond += 2
+
+    if ($value === '6' || $value === '9' || $value === '0') {
+        $rond = $rond +1;
+    } elseif ($value === '8') {
+        $rond = $rond +2;
+    }
+}
 
 // Write an action using echo().
-echo('answer');
+echo($rond);
